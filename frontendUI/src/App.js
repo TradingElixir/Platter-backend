61cc00defa94e98f52a419ddb78542c28c2db7ea
Siddhant Agnihotri <siddhant.ag7@gmail.com>
2023-01-30 23:28:46 +0530

    Fixed total portfolio value

--- frontendUI/src/App.js
+++ frontendUI/src/App.js
@@ -35,6 +35,7 @@ function App() {
               </div>
               <PortfolioValue
                 tokens={tokens}
+                nativeBalances={nativeBalances}
               />
             </>
           )}
--- frontendUI/src/components/PortfolioValue.js
+++ frontendUI/src/components/PortfolioValue.js
@@ -4,21 +4,18 @@ import "../App.css";
 
 function PortfolioValue({ tokens, nativeBalances }) {
   const [totalValue, setTotalValue] = useState(0);
-
-
-  console.log({ tokens, nativeBalances });
   
   useEffect(() => {
     if (Array.isArray(tokens) && Array.isArray(nativeBalances)) {
       
       let val = 0;
       for (let i = 0; i < tokens.length; i++) {
-        val = val + Number(tokens[i].value);
+        val = val + Number(tokens[i].val);
       }
       
       let val1 = 0;
       for (let i = 0; i < nativeBalances.length; i++) {
-        val1 = val1 + Number(nativeBalances[i].value);
+        val1 = val1 + Number(nativeBalances[i].nativeValue);
       }
       
       let val2 = 0;
